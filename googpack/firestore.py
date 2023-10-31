e7ac33e4c4da51a44cbd9a9023027041abadd45a
G Schaeffer <gschaeffer@gmail.com>
2023-10-31 15:13:05 -0500

    update firestore methods

--- googpack/firestore.py
+++ googpack/firestore.py
@@ -7,11 +7,24 @@ from pprint import pprint
 from google.cloud import firestore
 
 
-def get_config(config_env):
+def get_collection(collection_name):
     try:
         client = firestore.Client()
 
-        doc = client.collection("configs").document(config_env.lower()).get()
+        doc = client.collection(f"{collection_name}").get()
+        logging.info(f"Retrieved collection: {collection_name.lower()}")
+        return doc.to_dict()
+    except Exception as ex:
+        logging.error(f"Error: {ex}")
+
+    return None
+
+
+def get_document(collection_name, config_env):
+    try:
+        client = firestore.Client()
+
+        doc = client.collection(f"{collection_name}").document(config_env.lower()).get()
         logging.info(f"Retrieved config: {config_env.lower()}")
         return doc.to_dict()
     except Exception as ex:
